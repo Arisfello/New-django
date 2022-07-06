b1d287d22fad3003118dc3a4db56d47c52b1662f
arisafello <felomwansa@mail.com>
2022-07-06 15:59:22 +0300

    templates added

--- base/views.py
+++ base/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+rooms = [
+    {'id': 1, 'name': 'Lets Learn python!'},
+    {'id': 2, 'name': 'Design with me!'},
+    {'id': 3, 'name': 'Frontend developers!'},
+]
 
 
 def home(request):
-    return render(request, 'home.html')
+    context = {'room': rooms}
+    return render(request, 'home.html', context)
 
 
 def room(request):
     return render(request, 'room.html')
-
-
-
-
