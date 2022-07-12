18eb474040cc5137678f5ce6983ec8d2124bb294
arisafello <felomwansa@mail.com>
2022-07-12 15:21:03 +0300

    room form added

--- base/views.py
+++ base/views.py
@@ -15,10 +15,11 @@ def home(request):
 
 
 def room(request, pk):
-    # room = None
-    # for i in rooms:
-    #     if i['id'] == int(pk):
-    #         room = i
     room =Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
+
+
+def createRoom(request):
+    context = {}
+    return render(request, 'base/room_form.html', context )
