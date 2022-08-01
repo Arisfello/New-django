1628240b949409ae7dedac2cfcd047c4af7875d4
arisafello <felomwansa@mail.com>
2022-08-01 11:26:17 +0300

    Updated the delete function

--- base/views.py
+++ base/views.py
@@ -23,12 +23,13 @@ def createRoom(request):
             form.save()
             return redirect('home')
     context = {'form': form}
+    # return render(request, 'base/room_form.html', context)
     return render(request, 'base/room_form.html', context)
 
 
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
-    form = RoomForm(instance =room)
+    form = RoomForm(instance=room)
     if request.method == 'POST':
         form = RoomForm(request.POST, instance=room)
         if form.is_valid():
@@ -36,3 +37,11 @@ def updateRoom(request, pk):
             return redirect('home')
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
+
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': room})
